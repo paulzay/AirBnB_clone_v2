2be8f69063ff1c4ef1af7ebe1a5747b21754b9c8
Paul Ogolla <paulotieno2@gmail.com>
2024-05-16 22:22:54 +0300

    tgz

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,12 +9,12 @@ def do_pack():
     local("mkdir -p versions")
     timestring = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
     filename = '{}{}'.format("web_static_", timestring)
-    local(f"tar -cvf {filename}.tar web_static/")
-    archive_name = '{}{}'.format(filename, ".tar")
+    local(f"tar -cvf {filename}.tgz web_static/")
+    archive_name = '{}{}'.format(filename, ".tgz")
     # local(f"echo {archive_name}")
     archive_path = f"versions/{archive_name}"
 
     if archive_name:
-        local(f"mv {archive_name} /versions")
+        local(f"mv {archive_name} versions")
         return archive_path
     return None
