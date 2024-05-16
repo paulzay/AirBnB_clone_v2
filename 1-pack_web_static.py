2bc759e9d7f06b066e2109591dee50179ccae4e8
Paul Ogolla <paulotieno2@gmail.com>
2024-05-16 21:54:15 +0300

    2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """compressing with fabric"""
-from fabric.api import run
-import shutil
+from fabric.api import local
 import datetime
 
 
@@ -9,13 +8,13 @@ def do_pack():
     """function doc"""
     local("mkdir -p versions")
     timestring = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    filename = '{}{}'.format("web_static", timestring)
-    archive_name = local("shutil.make_archive(filename, 'tar', 'web_static')")
-    # shutil.make_archive(filename, 'tar', 'web_static')
-    # archive_name = f"web_static_{timestring}.tgz"
+    filename = '{}{}'.format("web_static_", timestring)
+    local(f"tar -cvf {filename}.tar web_static/")
+    archive_name = '{}{}'.format(filename,".tar")
+    # local(f"echo {archive_name}")
     archive_path = f"versions/{archive_name}"
 
     if archive_name:
-        local("mv archive_name /versions")
+        local(f"mv {archive_name} /versions")
         return archive_path
     return None
