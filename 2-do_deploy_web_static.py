1ddea03cd8b28fa0abf884d10e34a4b3bf1a8432
Paul Ogolla <paulotieno2@gmail.com>
2024-05-19 23:33:19 +0300

    update linter

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,6 +3,7 @@
 from fabric.api import run, put
 import os
 
+
 def do_deploy(archive_path):
     """documentation block"""
     env.hosts = ['18.234.192.140', '100.27.12.25']
@@ -19,16 +20,19 @@ def do_deploy(archive_path):
         put(archive_path, "/tmp/")
         run("mkdir -p /data/web_static/releases/{}".format(fl))
         # decompress the tgz
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(filename, fl))
+        run("tar -xzf /tmp/{} -C \
+            /data/web_static/releases/{}".format(filename, fl))
         # delete the archive from server
         run("sudo rm {}".format(filename))
-        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(filename, fl))
+        run("mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/".format(filename, fl))
         run("rm -rf /data/web_static/releases/{}/web_static".format(fl))
         # delete symbolic link
         run("rm -rf /data/web_static/current")
         # create new symbolic link
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(fl))
+        run('ln -s /data/web_static/releases/{}/ \
+            /data/web_static/current'.format(fl))
         print("New version deployed!")
         return True
-    except:
+    except ValueError:
         return False
