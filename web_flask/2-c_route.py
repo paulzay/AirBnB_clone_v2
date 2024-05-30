36fe41aa3370933c876ad99c894c87e1b89b6013
Paul Ogolla <paulotieno2@gmail.com>
2024-05-30 23:33:31 +0300

    task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_again():
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    return f"C {escape(text.replace("_", " "))}"
+
+if __name__ == "__main__":
+    app.run(debug=True)
