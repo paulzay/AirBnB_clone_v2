6fec7840bed12f6d0a4efa54b75b4ec23e4d84aa
Paul Ogolla <paulotieno2@gmail.com>
2024-06-01 00:37:46 +0300

    update

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
+"""docs"""
 
 from flask import Flask
 from markupsafe import escape
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
+    """home route"""
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hello_again():
+    """hbnb route"""
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
+    """c is fun"""
     return f"C {escape(text.replace("_", " "))}"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
