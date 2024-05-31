1b9aa168857b816ffaf1d6c75bf2e6f390796fd7
Paul Ogolla <paulotieno2@gmail.com>
2024-06-01 00:49:53 +0300

    updates

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,36 +1,48 @@
 #!/usr/bin/python3
+"""docs"""
 
 from flask import Flask, render_template
 from markupsafe import escape
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
+    """docs"""
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hello_again():
+    """docs"""
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
+    """docs"""
     return f"C {escape(text.replace("_", " "))}"
+
 
 @app.route('/python/<text>', strict_slashes=False)
 def python_is(text):
+    """docs"""
     return f"Python {escape(text.replace("_", " "))}"
+
 
 @app.route('/number<n>', strict_slashes=False)
 def only_nums(n):
-    if type(n) == int:
-      return f"{n} is a number"
+    """docs"""
+    if type(n) is int:
+        return f"{n} is a number"
+
 
 @app.route('/number_template/<n>', strict_slashes=False)
 def num_template(n):
+    """docs"""
     return render_template('5-number.html', n=n)
-    
+
 
 if __name__ == "__main__":
     app.run(debug=True)
- 
