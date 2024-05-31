6c9ab613271f371ae196c6407372fc091ffb5f94
Paul Ogolla <paulotieno2@gmail.com>
2024-06-01 00:40:23 +0300

    update

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,25 +1,35 @@
 #!/usr/bin/python3
+"""docs"""
 
 from flask import Flask
 from markupsafe import escape
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
+    """home"""
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hello_again():
+    """hbnb"""
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
+    """c is fun"""
     return f"C {escape(text.replace("_", " "))}"
+
 
 @app.route('/python/<text>', strict_slashes=False)
 def python_is(text):
+    """py is cool"""
     return f"Python {escape(text.replace("_", " "))}"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
