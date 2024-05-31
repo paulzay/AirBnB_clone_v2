e082eb481867d60728d12cb3fd9c07ec4316be3f
Paul Ogolla <paulotieno2@gmail.com>
2024-06-01 00:34:00 +0300

    docs

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
+"""docs"""
 
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
+    """home route"""
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hello_again():
+    """hbnb route"""
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
