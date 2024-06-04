b0a24740c81458a291ccc299623e56a3f65cdfff
Paul Ogolla <paulotieno2@gmail.com>
2024-06-04 16:48:43 +0300

    fix

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -24,6 +24,7 @@ def c_is_fun(text):
     return "C " + text.replace("_", " ")
 
 
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_is(text):
     """py is cool"""
